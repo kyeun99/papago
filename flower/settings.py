be5920242a25fded976f37fa1c4d9462faaab027
yuminko <yuk041@ucsd.edu>
2019-05-13 21:37:29 +0900

    yumin yeun

--- flower/settings.py
+++ flower/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '95mc+3vb$+(&gi8%h&##on43$6aatq7&x3ezploarbjw*0riaq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
